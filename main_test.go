ea5f4bf7f4a7894cb9973a387776354cbd72c3dc
Arnau Diaz <diazarnau0@gmail.com>
2024-03-14 19:36:27 +0100

    protected

--- main_test.go
+++ main_test.go
@@ -24,11 +24,23 @@ func TestMain(t *testing.T) {
 
 	t.Run("it should return error on GET", func(t *testing.T) {
 		_, err := http.Get("http://localhost:8081")
-		assert.NoError(t, err)
+		assert.Error(t, err)
 		assert.ErrorContains(t, err, "connection refused")
 	})
 
 	t.Run("it should return unauthorized when API-key not presentGET", func(t *testing.T) {
-		t.Fatal("todo")
+		client := http.DefaultClient
+		req, err := http.NewRequest("GET", "http://localhost:8080/v1/protected", nil)
+		assert.NoError(t, err)
+		if err != nil {
+			panic(err)
+		}
+
+		req.Header.Set("API-Key", "your-api-key")
+
+		resp, err := client.Do(req)
+		defer resp.Body.Close()
+
+		assert.NoError(t, err)
 	})
 }
